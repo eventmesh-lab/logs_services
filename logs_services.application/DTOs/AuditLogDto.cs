1de92995eccd1fb4aa39a9556470e60d4afb6a83
crismarygonzalez <166092453+crismarygonzalez@users.noreply.github.com>
2026-01-15 04:37:51 -0400

    se agregaron los casos de uso necesarios para logs services

---
+++ logs_services.application/DTOs/AuditLogDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logs_services.application.DTOs
+{
+    public class AuditLogDto
+    {
+        public string Id { get; set; }
+        public Guid EventoId { get; set; }
+        public string ServicioOrigen { get; set; }
+        public string UsuarioId { get; set; }
+        public string TipoAccion { get; set; }
+        public object Datos { get; set; } // Puede ser JSON crudo o un objeto
+        public DateTime FechaOcurrencia { get; set; }
+        public string Nivel { get; set; }
+    }
+}
