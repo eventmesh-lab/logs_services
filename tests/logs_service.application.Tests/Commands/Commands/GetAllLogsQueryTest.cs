6cf130a101faa3946cc58b3a0b807e5e291ea0d5
Angelbg1707 <166092453+crismarygonzalez@users.noreply.github.com>
2026-01-15 14:55:13 -0400

    Tests
    Se realizaron pruebas unitarias

---
+++ tests/logs_service.application.Tests/Commands/Commands/GetAllLogsQueryTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using logs_services.application;
+using logs_services.application.DTOs;
+using MediatR;
+using System.Collections.Generic;
+
+namespace logs_services.application.Tests.Queries
+{
+    public class GetAllLogsQueryTests
+    {
+        [Fact]
+        public void Constructor_ShouldInitializeCorrectly()
+        {
+            var query = new GetAllLogsQuery();
+
+            Assert.NotNull(query);
+            Assert.IsAssignableFrom<IRequest<List<AuditLogDto>>>(query);
+        }
+    }
+}
